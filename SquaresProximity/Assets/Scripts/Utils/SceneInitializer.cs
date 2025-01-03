d18b1a3ef298d0e891b586195d3262834fc47837
Agasthyeaswar Bhanu Upadhyayula <uabhanu@gmail.com>
2025-01-03 17:46:17 +0000

    Removed Photon code completely. Hooking up Join Lobby Button Up Next.

--- SquaresProximity/Assets/Scripts/Utils/SceneInitializer.cs
+++ SquaresProximity/Assets/Scripts/Utils/SceneInitializer.cs
@@ -1,73 +1,25 @@
 namespace Utils
 {
-    using Managers;
-    using Misc;
-    using Photon.Pun;
     using System.Collections.Generic;
     using UnityEngine;
 
     public class SceneInitializer : MonoBehaviour
     {
-        private Dictionary<string, int> _platformToIndex = new()
+        private Dictionary<string , int> _platformToIndex = new()
         {
-            { "UNITY_STANDALONE", 0 },
-            { "UNITY_ANDROID", 1 },
-            { "UNITY_WEBGL", 2 }
+            { "UNITY_STANDALONE" , 0 },
+            { "UNITY_ANDROID" , 1 },
+            { "UNITY_WEBGL" , 2 }
         };
 
-        [SerializeField] private bool isOnlineMultiplayer; // TODO Dynamically set this in production
         [SerializeField] private List<GameObject> sceneObjectsList;
         [SerializeField] private List<GameObject> uiObjectsList;
 
         private void Start()
         {
-            if(isOnlineMultiplayer)
-            {
-                Debug.Log("Initializing online mode...");
-            }
-            else
-            {
-                Debug.Log("Initializing offline mode...");
-            }
-            
-            InstantiateObjects(sceneObjectsList);
-            
-            string currentPlatform = GetPlatformName();
-            
-            if(_platformToIndex.ContainsKey(currentPlatform))
-            {
-                int index = _platformToIndex[currentPlatform];
-                GameObject uiPrefab = uiObjectsList[index];
-                InstantiateObjects(new List<GameObject> { uiPrefab });
-            }
-            else
-            {
-                Debug.LogError($"Platform not supported for UI object instantiation: {currentPlatform}");
-            }
-
+            InstantiateSceneObjects();
+            InstantiateUIObjects();
             Destroy(gameObject);
-        }
-        
-        private void AddPhotonView(GameObject obj)
-        {
-            if(obj.GetComponent<PhotonView>() == null)
-            {
-                obj.AddComponent<PhotonView>();
-                Debug.Log($"PhotonView dynamically added to: {obj.name}");
-            }
-        }
-
-        private void InstantiateObjects(List<GameObject> objectList)
-        {
-            foreach(var prefab in objectList)
-            {
-                GameObject instantiatedObject = isOnlineMultiplayer ? PhotonInstantiate(prefab) : Instantiate(prefab);
-
-                if(isOnlineMultiplayer && NeedsPhotonView(prefab) && instantiatedObject.GetComponent<PhotonView>() == null)
-                {
-                    AddPhotonView(instantiatedObject);
-                }
-            }
         }
 
         private string GetPlatformName()
@@ -85,36 +37,27 @@ namespace Utils
             #endif
         }
 
-        private bool NeedsPhotonView(GameObject obj)
+        private void InstantiateSceneObjects()
         {
-            var photonViewRequiredComponents = new[]
+            foreach(var sceneObj in sceneObjectsList)
             {
-                typeof(InGameUIManager),
-                typeof(PlayerController),
-                typeof(GameManager),
-                typeof(GridManager),
-                typeof(ScoreManager)
-            };
-
-            foreach (var componentType in photonViewRequiredComponents)
-            {
-                if(obj.GetComponent(componentType) != null)
-                {
-                    return true;
-                }
+                Instantiate(sceneObj);
             }
-
-            return false;
         }
 
-        private GameObject PhotonInstantiate(GameObject prefab)
+        private void InstantiateUIObjects()
         {
-            if(PhotonNetwork.IsConnected && isOnlineMultiplayer)
-            {
-                return PhotonNetwork.Instantiate(prefab.name , Vector3.zero , Quaternion.identity);
-            }
+            string currentPlatform = GetPlatformName();
 
-            return Instantiate(prefab);
+            if(_platformToIndex.ContainsKey(currentPlatform))
+            {
+                int index = _platformToIndex[currentPlatform];
+                Instantiate(uiObjectsList[index]);
+            }
+            else
+            {
+                Debug.LogError($"Platform not supported for UI object instantiation : {currentPlatform}");
+            }
         }
     }
 }
